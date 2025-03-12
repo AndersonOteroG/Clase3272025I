d3d06d8256561f0e3a29766a2943cf71ed5eb67e
AndersonOteroG <aoterog@estudiante.uniajc.edu.co>
2025-03-12 09:11:19 -0500

    Add files via upload

---
+++ ParcialMultiplo.java
@@ -0,0 +1,24 @@
+package Pacial;
+
+import java.util.Scanner;
+
+public class ParcialMultiplo {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        // Entrada de datos
+        System.out.print("Ingrese el primer número: ");
+        int numeroUno = scanner.nextInt();
+        System.out.print("Ingrese el segundo número: ");
+        int numeroDos = scanner.nextInt();
+
+        // Verificación de múltiplos
+        if (numeroUno % numeroDos == 0) {
+            System.out.println(numeroUno + " es múltiplo de " + numeroDos);
+        } else if (numeroDos % numeroUno == 0) {
+            System.out.println(numeroDos + " es múltiplo de " + numeroUno);
+        } else {
+            System.out.println("Ninguno de los dos números es múltiplo del otro.");
+        }
+    }
+}
