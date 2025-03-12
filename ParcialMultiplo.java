1efc4ac072abf9b129a7ebb841cd4cb7652f3fa6
AndersonOteroG <aoterog@estudiante.uniajc.edu.co>
2025-03-12 10:39:15 -0500

    ParcialMultiplo.java

--- ParcialMultiplo.java
+++ ParcialMultiplo.java
@@ -6,19 +6,17 @@ public class ParcialMultiplo {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        // Entrada de datos
         System.out.print("Ingrese el primer número: ");
         int numeroUno = scanner.nextInt();
         System.out.print("Ingrese el segundo número: ");
         int numeroDos = scanner.nextInt();
 
-        // Verificación de múltiplos
         if (numeroUno % numeroDos == 0) {
-            System.out.println(numeroUno + " es múltiplo de " + numeroDos);
+            System.out.println(numeroUno + " es multiplo de " + numeroDos);
         } else if (numeroDos % numeroUno == 0) {
-            System.out.println(numeroDos + " es múltiplo de " + numeroUno);
+            System.out.println(numeroDos + " es multiplo de " + numeroUno);
         } else {
-            System.out.println("Ninguno de los dos números es múltiplo del otro.");
+            System.out.println("No son multiplos entre si.");
         }
         scanner.close();
 
