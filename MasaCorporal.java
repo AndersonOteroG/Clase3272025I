4143c0dfdaee8b6cfcef7ff7a1a31e8da97e680a
AndersonOteroG <aoterog@estudiante.uniajc.edu.co>
2025-04-09 17:19:40 -0500

    MasaCorporal.java

--- MasaCorporal.java
+++ MasaCorporal.java
@@ -14,12 +14,22 @@ public class MasaCorporal {
     public float calcularIMC() {
         masa = peso / (altura * altura);
 
-        if (masa < 18.5) {
-            System.out.println("Tienes bajo peso.");
-        } else if (masa >= 18.5 && masa <= 24.9) {
-            System.out.println("Tienes un peso normal.");
+        if (masa < 16) {
+            System.out.println("delgadez severa.");
+        } else if (masa >= 16 && masa <= 17) {
+            System.out.println("delgadez moderada.");
+        } else if (masa >= 17 && masa <= 18.5) {
+            System.out.println("Delgadez leve");
+        } else if (masa >= 18.5 && masa < 25) {
+            System.out.println("Peso normal");
+        } else if (masa >= 25 && masa < 30) {
+            System.out.println("Sobrepeso");
+        } else if (masa >= 30 && masa < 35) {
+            System.out.println("Obesidad leve");
+        } else if (masa >= 35 && masa < 40) {
+            System.out.println("Obesidad moderada");
         } else {
-            System.out.println("Tienes sobrepeso.");
+            System.out.println("Obesidad mórbida");
         }
 
         return masa;
